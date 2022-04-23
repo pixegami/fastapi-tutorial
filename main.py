e7dc956583c7ac0225217eb25da370bb2ed79f46
pixegami <pixegami@gmail.com>
2022-04-23 16:10:34 +1000

    add example API code

---
+++ main.py
@@ -0,0 +1,75 @@
+import json
+import os
+from typing import Literal, Optional
+from uuid import uuid4
+from fastapi import FastAPI, HTTPException, Path
+import random
+from fastapi.encoders import jsonable_encoder
+from pydantic import BaseModel
+
+
+class Book(BaseModel):
+    name: str
+    genre: Literal["fiction", "non-fiction"]
+    price: float
+    book_id: Optional[str] = uuid4().hex
+
+
+BOOKS_FILE = "books.json"
+BOOKS = []
+
+if os.path.exists(BOOKS_FILE):
+    with open(BOOKS_FILE, "r") as f:
+        BOOKS = json.load(f)
+
+
+app = FastAPI()
+
+
+@app.get("/")
+async def home():
+    pass
+
+
+@app.get("/")
+async def root():
+    return {"message": "Welcome to my fitness app!"}
+
+
+@app.get("/random-book")
+async def random_book():
+    return random.choice(BOOKS)
+
+
+@app.get("/list-books")
+async def list_books():
+    return {"books": BOOKS}
+
+
+@app.get("/book_by_index/{index}")
+async def book_by_index(index: int):
+    if index < len(BOOKS):
+        return BOOKS[index]
+    else:
+        raise HTTPException(404, f"Book index {index} out of range ({len(BOOKS)}).")
+
+
+@app.post("/add-book")
+async def add_book(book: Book):
+    book.book_id = uuid4().hex
+    json_book = jsonable_encoder(book)
+    BOOKS.append(json_book)
+
+    with open(BOOKS_FILE, "w") as f:
+        json.dump(BOOKS, f)
+
+    return {"book_id": book.uuid}
+
+
+@app.get("/get-book")
+async def get_book(book_id: str):
+    for book in BOOKS:
+        if book.book_id == book_id:
+            return book
+
+    raise HTTPException(404, f"Book ID {book_id} not found in database.")
